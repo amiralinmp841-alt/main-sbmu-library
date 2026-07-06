fdb53225a18e4e66c1df93d2085938c7bb19076a
amiralinmp841-alt <amiralinmp841@gmail.com>
2026-07-06 05:03:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -4144,6 +4144,7 @@ async def handle_navigation(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 }
         
             i += 1
+            return CHOOSING
         
 
     # ======= Admin panel handling END ======= ======= Admin panel handling END ======= ======= Admin panel handling END ======= ======= Admin panel handling END ======= ===
